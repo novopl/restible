7176eb064fe01e4d2db91fb4bb1938612407152c
Mateusz Klos <novopl@gmail.com>
2018-06-08 00:57:18 +0100

    Actions + experimental 3rd party integrations

--- src/restible/core/routing.py
+++ src/restible/core/routing.py
@@ -22,3 +22,59 @@ def make_urls(default_endpoint_cls, endpoints):
             urls += endpoint.urls
 
     return urls
+
+
+class api_route(object):
+    """ Decorator for defining API routs.
+
+    API routes are endpoints that do not belong to any resource.
+    """
+    ROUTE_ATTR = '__api_route__'
+
+    def __init__(self, methods=None):
+        self.meta = jsobj({
+            'methods': methods or ['post']
+        })
+
+    def __call__(self, fn):
+        """ Decorator. """
+        setattr(fn, self.ROUTE_ATTR, self.meta)
+        return fn
+
+    @classmethod
+    def get_meta(cls, route):
+        """ Get route metadata. """
+        return getattr(route, cls.ROUTE_ATTR, {})
+
+
+class api_action(object):
+    """ Decorator for defining resource actions.
+
+    Actions are extra HTTP endpoints that can be called with POST.
+    """
+    ACTION_ATTR = '__api_action__'
+
+    def __init__(self, name=None, generic=False, protected=True):
+        self.name = name
+        self.generic = generic
+        self.protected = protected
+
+    def __call__(self, fn):
+        """ Decorator. """
+        setattr(fn, self.ACTION_ATTR, jsobj({
+            'name': self.name or fn.__name__,
+            'generic': self.generic,
+            'method_name': fn.__name__,
+            'protected': self.protected,
+        }))
+        return fn
+
+    @classmethod
+    def is_action(cls, obj):
+        """ Return true if obj is an API action. """
+        return hasattr(obj, cls.ACTION_ATTR)
+
+    @classmethod
+    def get_meta(cls, action):
+        """ Get action metadata """
+        return getattr(action, cls.ACTION_ATTR, {})
---
+++ src/restible/sqlalchemy/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+""" SQLAlchemy integration. """
+from __future__ import absolute_import, unicode_literals
