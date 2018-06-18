73c8cdd444414d1eaaa26d4599963d20acec11ac
Mateusz Klos <novopl@gmail.com>
2018-06-18 23:24:52 +0100

    Release: v0.2.14

--- src/restible/__init__.py
+++ src/restible/__init__.py
@@ -15,7 +15,7 @@ from .core.resource import RestResource
 from .core.routing import api_action
 from .core.routing import api_route
 
-__version__ = '0.2.13'
+__version__ = '0.2.14'
 
 __all__ = [
     'api_action',
