5fdc29609ed5fd3c7c33966b28614724a213a488
novo <novopl@gmail.com>
2018-08-28 01:47:28 +0100

    Releasing v0.3.2

--- src/restible/__init__.py
+++ src/restible/__init__.py
@@ -16,7 +16,7 @@ from .core.resource import RestResource
 from .core.routing import api_action
 from .core.routing import api_route
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 __all__ = [
     'api_action',
