1603761d4b355bdbb755e5d278f1b4c7ece83c8a
Mateusz Klos <novopl@gmail.com>
2019-04-08 01:58:35 +0100

    Releasing v0.11.6

--- src/restible/__init__.py
+++ src/restible/__init__.py
@@ -35,7 +35,7 @@ from .resource import RestResource
 from .actions import api_action
 from .routing import api_route
 
-__version__ = '0.11.5'
+__version__ = '0.11.6'
 
 __all__ = [
     'api_action',
